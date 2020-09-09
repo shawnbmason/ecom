5d9eb013cbc8c3b0e9c009f5aa76cd06e9ee8a6d
shawnbmason <s.mason1993@yahoo.com>
2020-09-08 21:15:23 -0400

    developed navbar and header actions for setting new links and used in account.js

--- src/components/account/account.js
+++ src/components/account/account.js
@@ -1,18 +1,60 @@
 import React, { Component } from 'react';
 
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
+import AccountInformation from './accountInformation';
+import PurchaseHistory from './purchaseHistory';
+
 class Account extends Component {
 
-  renderContent() {
-    return <h1> content </h1>
+  componentDidMount() {
+
+    const navbarLinks = [
+      {
+        _id: 0,
+        title: 'Purchase history',
+        active: false,
+        component: <PurchaseHistory/>
+      },
+      {
+        _id: 1,
+        title: 'Acount Information',
+        active: true,
+        component: <AccountInformation/>
+      }
+    ]
+
+    this.props.setHeaderLinks([]);
+    this.props.setNavbarLinks(navbarLinks);
   }
 
-  render() {
-    return (
-      <div>
-          {this.renderContent}
-      </div>
+  renderContent() {
+    let jsx;
+      if(this.props.navbarLinks) {
+        this.props.navbarLinks.forEach(link => {
+          if(link.active) {
+            jsx = link.component;
+            }
+          })
+        }
+        return jsx;
+    }
+
+    render() {
+      return (
+        <div>
+          { this.renderContent() }
+        </div>
     )
   }
 }
+
+function mapStateToProps(state) {
+    const { headerLinks, navbarLinks } = state.headerNavbar;
+    return { headerLinks, navbarLinks }
+}
+
+Account = connect(mapStateToProps, actions)(Account);
 
 export default Account;
