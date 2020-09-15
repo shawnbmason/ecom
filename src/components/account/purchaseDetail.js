f029edb456ae2542f269b469b6b78a66a3bb2cf8
shawnbmason <s.mason1993@yahoo.com>
2020-09-15 18:36:03 -0400

    fixed bug setting user purchases Ids all the same

--- src/components/account/purchaseDetail.js
+++ src/components/account/purchaseDetail.js
@@ -15,7 +15,7 @@ class PurchaseDetail extends Component {
   render() {
     const { className, orderNumber, orderDate, user, total, creditCard } = this.props;
     const { name, shippingAddress } = user;
-    const nameAddress = `${name}       ${shippingAddress}`
+    const nameAddress = `${name}--------- ${shippingAddress}`
 
     return (
       <div className={`${className} purchase-detail`}>
