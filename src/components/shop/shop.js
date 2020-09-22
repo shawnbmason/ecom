0d5590209ea708101366c9d866acc93295c333a9
shawnbmason <s.mason1993@yahoo.com>
2020-09-22 18:18:59 -0400

    built base for shop component. built shop route etc

---
+++ src/components/shop/shop.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
+class Shop extends Component {
+
+  componentDidMount() {
+    const headerLinks = [
+      {
+        _id: 0,
+        title: 'Login',
+        path: '/signin'
+      }
+    ]
+    this.props.setHeaderLinks(headerLinks);
+    // fetch shop product action creator
+    // set the header links
+    // fetch navbar links
+    // set the navbar links
+    // filter products with links
+  }
+  render() {
+    return (
+      <div className='shop'>
+        // shop search bar
+        // shop product
+        // shop cart button
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return { state }
+}
+
+Shop = connect(mapStateToProps, actions)(Shop);
+
+export default Shop;
